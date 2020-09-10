e3ff8cdd5b0fbb1d3cb76fd1425b08b90897dd2a
k8gwen <66880716+k8gwen@users.noreply.github.com>
2020-09-10 15:15:48 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let currentImageIndex = 0
 const images = ['/images/Bird.png','images/abstract_paint.jpg','images/Bear.jpg','images/Mom_Toon_Me.jpeg','images/bubble_sketch.jpg','images/Dusky.JPG','images/denver_paint.jpg','images/dream_doodle.png','images/Gabe_Toon_Me.png','images/Something_Rad_.png','images/triangle_paint.jpg','images/Somethin_Rad.png']
-
-let next = document.getElementById('next')
 let image = document.getElementById('image')
+let next = document.getElementById('next')
+if(next) {
 next.addEventListener('click',() => {
     if (currentImageIndex == images.length-1) {
         currentImageIndex = 0
@@ -10,18 +10,20 @@ next.addEventListener('click',() => {
     } else {
         currentImageIndex += 1
         image.src = images[currentImageIndex]
-    }
-})
+        }
+    })
+}
 let prev = document.getElementById('prev')
+if(prev){
 prev.addEventListener('click',() => {
     if (currentImageIndex <= 0){
         currentImageIndex = images.length-1
     } else {
         currentImageIndex -= 1
         image.src = images[currentImageIndex]   
-    }
-     
-})
+        }
+    })
+}
 
 
 
